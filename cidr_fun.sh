4ba355fae6023e8835ff3ed8c803316158135e77
KMICB <86378435+kmicb@users.noreply.github.com>
2023-06-03 18:58:27 -0400

    added bulk of main menu opt01 shell and sub main menu

--- cidr_fun.sh
+++ cidr_fun.sh
@@ -4,16 +4,16 @@
 
 ## features
 # Get CIDRs...
-    # List Entities (set to array if needed)
-        # AWS
-        # Google
-        # Cloudflare
-        # Apple
-        # Microsoft
-        # Meta
-            # Facebook
-            # Instagram
-        # Snap
+# List Entities (set to array if needed)
+# AWS
+# Google
+# Cloudflare
+# Apple
+# Microsoft
+# Meta
+# Facebook
+# Instagram
+# Snap
 # get list of ipv4 and ipv6 cidrs individualally
 # search for a cidr in all places
 
@@ -34,13 +34,6 @@
 # output ipv6 cloudflare cidrs
 # curl https://www.cloudflare.com/ips-v6 | sort
 ########################################################################################
-
-
-
-
-
-
-
 
 # Main Menu code block starts here
 # menu items
@@ -67,31 +60,93 @@ read_option() {
     echo ""
     case $choice in
     1) # List CIDRs...
-        opt1_entity_items=(
-            "1: AWS"
-            "2: Google"
-            "3: Apple"
-            "4: Microsoft"
-            "5: Snap"
-            "6: Facebook"
-            "7: Insta"
-            "8: Meta"
-            "x: Exit"
-        )
-        # print menu
-        opt1_print_menu() {
-            echo ""
-            for item in "${opt1_entity_items[@]}"; do
-                echo "$item"
+        while true; do
+            opt01_entity_items=(
+                "1: AWS"
+                "2: Google"
+                "3: Apple"
+                "4: Microsoft"
+                "5: Snap"
+                "6: Facebook"
+                "7: Insta"
+                "8: Meta"
+                "m: Main Menu"
+                "x: Exit"
+            )
+            # print menu
+            opt01_print_menu() {
+                echo ""
+                for item in "${opt01_entity_items[@]}"; do
+                    echo "$item"
+                done
+                echo ""
+            }
+
+            # Entity options case menu
+            opt01_print_menu
+            read -rp "Option: " opt01_choice
+            case $opt01_choice in
+            1) # AWS
+                # prompt ipv4, ipv6, or all - I bet this coiuld be a case block too for the menu instead of echos...
+                echo "AWS was chosen."
+                ;;
+            2) # Google
+                # TODO: add code for Google
+                ;;
+            3) # Apple
+                # TODO: add code for Apple
+                ;;
+            4) # Microsoft
+                # TODO: add code for Microsoft
+                ;;
+            5) # Snap
+                # TODO: add code for Snap
+                ;;
+            6) # Facebook
+                # TODO: add code for Facebook
+                ;;
+            7) # Insta
+                # TODO: add code for Insta
+                ;;
+            8) # Meta
+                # TODO: add code for Meta
+                ;;
+            m) # Back to main menu
+                break
+                ;;
+            x) # Quit
+                echo "Aborting script."
+                echo ""
+                exit 0
+                ;;
+            *) # catch-all
+                echo "Invalid entry."
+                echo ""
+                ;;
+            esac
+
+            # prompt to go back to the main menu or quit
+            while true; do
+                read -rp "Return to menu or quit? (m/q): " back_or_quit
+                echo ""
+                case $back_or_quit in
+                m) # Back to menu
+                    break
+                    ;;
+                q) # Quit
+                    echo "Aborting script."
+                    echo ""
+                    exit 0
+                    ;;
+                *) # catch-all
+                    echo "Invalid entry."
+                    echo ""
+                    ;;
+                esac
             done
-            echo ""
-        }
-        # prompt aws, google, apple, microsoft, etc
-        # prompt ipv4 or ipv6 or all
-        # display items
-        # ask if menu or quit
+        done
         ;;
-    2)  # Search CIDRs...
+    2) # Search CIDRs...
         # prompt for IP/CIDR
         # go out and find info
         # display info
